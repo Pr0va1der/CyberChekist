5330e3556b4641ee521be8e09e05c905f6618100
Pr0va1der <sasha2002radaev.com@gmail.com>
2020-08-10 13:52:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -32,15 +32,11 @@ def kick_member(user_id, chat_id, messages=None):
 
 # Проверка пользователя на ересь в подписках
 def check_users(users, chat_id):
-    print('213')
     vk = vk_auth()
     groups = read_file()
-    print(groups)
     for user in users:
-        print(user)
         for group in groups:
             is_member = vk.groups.isMember(group_id=group, user_id=user)
-            print(is_member)
             if white_list(user):
                 break
             elif is_member:
@@ -113,8 +109,105 @@ def user_in_list(list, user):
     return flag
 
 
+def is_difference(init, finite, chat_id, from_id, is_kicked):
+    if finite and init != finite:
+        if finite['count'] < init['count']:
+            new_user = find_difference(init['items'], finite['items'])
+            check_users(new_user, chat_id)
+        elif finite['count'] > init['count']:
+            if not is_kicked and not sender_is_admin(init, from_id):
+                leave_user = find_difference(finite['items'], nit['items'])
+                for user in leave_user:
+                    kick_member(user, chat_id)
+
+
+def command_help(chat_id):
+    with open('help_commands.txt', 'r', encoding='UTF-8') as file:
+        messages = file.read()
+        send_vk(messages, chat_id)
+
+
+def command_online(list):
+    vk = vk_auth()
+    users_list = vk.messages.getConversationMembers(peer_id=list['peer_id'], group_id=list['group_id'])
+    online_users = 'Список пользователей в сети:\n'
+    for user in users_list['profiles']:
+        if user['online']:
+            if user['online_info']['is_mobile']:
+                online_status = 'через телефон\n'
+                device_status = '📱'
+            else:
+                online_status = 'через сайт\n'
+                device_status = '🖥'
+            online_users += f'•{device_status}[{user["screen_name"]}|{user["first_name"]} {user["last_name"]}] {online_status}'
+    send_vk(online_users, list['chat_id'], is_alert=1)
+
+
+def command_flip(chat_id):
+    if randint(0, 1):
+        random_flip = 'Орел'
+    else:
+        random_flip = 'Решка'
+    messages = f'Тебе выпало: {random_flip}'
+    send_vk(messages, chat_id)
+
+
+def command_roll(text, chat_id):
+    if len(text.lower().split()) > 1:
+        if text.lower().split()[1].isdigit():
+            if int(text.lower().split()[1]) < 1000:
+                messages = f'Случайное число: {randint(1, int(text.lower().split()[1]))}'
+                send_vk(messages, chat_id)
+            else:
+                messages = 'Слишком большое число. Повторите попытку'
+                send_vk(messages, chat_id)
+        else:
+            messages = 'Вы ввели неверное число. Повторите попытку'
+            send_vk(messages, chat_id)
+    else:
+        messages = 'Вы не ввели число. Повторите попытку'
+        send_vk(messages, chat_id)
+
+
+def command_who(list):
+    vk = vk_auth()
+    users_list = vk.messages.getConversationMembers(peer_id=list['peer_id'], group_id=list['group_id'])
+    who_number = randint(1, len(users_list['profiles'])) - 1
+    users = users_list['profiles']
+    user = f'[{users[who_number]["screen_name"]}|{users[who_number]["first_name"]} {users[who_number]["last_name"]}]'
+    messages = f'Это {user}'
+    send_vk(messages, list['chat_id'], is_alert=1)
+
+
+def command_kick(text, list):
+    vk = vk_auth()
+    if len(text.lower().split()) > 1:
+        if text.lower().split()[1].split('/')[0] == 'https:' and \
+                text.lower().split()[1].split('/')[2] == 'vk.com':
+            users_list = vk.messages.getConversationMembers(peer_id=list['peer_id'], group_id=list['group_id'])
+            user_nick = text.lower().split()[1].split('/')[3]
+            user_id = vk.utils.resolveScreenName(screen_name=user_nick)['object_id']
+            user_in_list(users_list, user_id)
+            if user_in_list(users_list, user_id):
+                if can_kick(users_list, list['from_id'], user_id, list['chat_id']):
+                    vk.messages.removeChatUser(chat_id=list['chat_id'], member_id=user_id)
+                    is_kicked = True
+                    return is_kicked
+            else:
+                messages = 'Пользователь не участник беседы'
+                send_vk(messages, list['chat_id'])
+        else:
+            messages = 'Неверная ссылка. Повторите попытку'
+            send_vk(messages, list['chat_id'])
+    else:
+        messages = 'Вы не ввели ссылку. Повторите попытку'
+        send_vk(messages, list['chat_id'])
+
+
 def main():
+    global data_list
     try:
+
         is_kicked = None
         group_id = '197440489'
         vk_session = vk_api.VkApi(token=token)
@@ -125,23 +218,16 @@ def main():
         for event in longpoll.listen():
             try:
                 peer_id = 2000000000 + int(event.chat_id)
-                chat_id = event.chat_id
                 is_chat = True
+                data_list = {'group_id': '197440489', 'chat_id': event.chat_id, 'peer_id': peer_id, 'from_id': event.obj.from_id}
             except:
                 user_id = event.obj.user_id
                 is_chat = False
+
             if is_chat:
-                users_init = vk.messages.getConversationMembers(peer_id=peer_id, group_id=group_id)
-                if users_finite and users_init != users_finite:
-                    if users_finite['count'] < users_init['count']:
-                        new_user = find_difference(users_init['items'], users_finite['items'])
-                        check_users(new_user, chat_id)
-                    elif users_finite['count'] > users_init['count']:
-                        if not is_kicked and not sender_is_admin(users_init, event.obj.from_id):
-                            leave_user = find_difference(users_finite['items'], users_init['items'])
-                            for user in leave_user:
-                                kick_member(user, chat_id)
-                users_finite = vk.messages.getConversationMembers(peer_id=peer_id, group_id=group_id)
+                users_init = vk.messages.getConversationMembers(peer_id=data_list['peer_id'], group_id=data_list['group_id'])
+                is_difference(users_init, users_finite, data_list['chat_id'], data_list['from_id'], is_kicked)
+                users_finite = vk.messages.getConversationMembers(peer_id=data_list['peer_id'], group_id=data_list['group_id'])
                 is_kicked = False
 
             if event.type == VkBotEventType.MESSAGE_NEW and event.obj.text:
@@ -149,77 +235,21 @@ def main():
                 if is_chat:
 
                     if event.obj.text.lower().split()[0] == '/помощь':
-                        with open('help_commands.txt', 'r', encoding='UTF-8') as file:
-                            messages = file.read()
-                            send_vk(messages, chat_id)
+                        command_help(data_list['chat_id'])
                     elif event.obj.text.lower().split()[0] == '/онлайн':
-                        users_list = vk.messages.getConversationMembers(peer_id=peer_id, group_id=group_id)
-                        online_users = 'Список пользователей в сети:\n'
-                        for user in users_list['profiles']:
-                            if user['online']:
-                                if user['online_info']['is_mobile']:
-                                    online_status = 'через телефон\n'
-                                    device_status = '📱'
-                                else:
-                                    online_status = 'через сайт\n'
-                                    device_status = '🖥'
-                                online_users += f'•{device_status}[{user["screen_name"]}|{user["first_name"]} {user["last_name"]}] {online_status}'
-                        send_vk(online_users, chat_id, is_alert=1)
+                        command_online(data_list)
                     elif event.obj.text.lower().split()[0] == '/монетка':
-                        if randint(0, 1):
-                            random_flip = 'Орел'
-                        else:
-                            random_flip = 'Решка'
-                        messages = f'Тебе выпало: {random_flip}'
-                        send_vk(messages, chat_id)
+                        command_flip(data_list['chat_id'])
                     elif event.obj.text.lower().split()[0] == '/ролл':
-                        try:
-                            if event.obj.text.lower().split()[1]:
-                                if int(event.obj.text.lower().split()[1]) < 1000:
-                                    try:
-                                        messages = f'Случайное число: {randint(1, int(event.obj.text.lower().split()[1]))}'
-                                        send_vk(messages, chat_id)
-                                    except:
-                                        messages = 'Вы ввели неверное число. Повторите попытку'
-                                        send_vk(messages, chat_id)
-                                else:
-                                    messages = 'Слишком большое число. Повторите попытку'
-                                    send_vk(messages, chat_id)
-                        except:
-                            messages = 'Вы не ввели число. Повторите попытку'
-                            send_vk(messages, chat_id)
+                        command_roll(event.obj.text, data_list['chat_id'])
                     elif event.obj.text.lower().split()[0] == '/кто':
-                        vk = vk_auth()
-                        users_list = vk.messages.getConversationMembers(peer_id=peer_id, group_id=group_id)
-                        who_number = randint(1, len(users_list['profiles'])) - 1
-                        users = users_list['profiles']
-                        user = f'[{users[who_number]["screen_name"]}|{users[who_number]["first_name"]} {users[who_number]["last_name"]}]'
-                        messages = f'Это {user}'
-                        send_vk(messages, chat_id, is_alert=1)
+                        command_who(data_list)
                     elif event.obj.text.lower().split()[0] == '/шанс':
                         messages = f'Вероятность - {randint(1, 100)}%'
-                        send_vk(messages, chat_id)
+                        send_vk(messages, data_list['chat_id'])
                     elif event.obj.text.lower().split()[0] == '/кик':
-                        if len(event.obj.text.lower().split()) > 1:
-                            if event.obj.text.lower().split()[1].split('/')[0] == 'https:' and \
-                                    event.obj.text.lower().split()[1].split('/')[2] == 'vk.com':
-                                users_list = vk.messages.getConversationMembers(peer_id=peer_id, group_id=group_id)
-                                user_nick = event.obj.text.lower().split()[1].split('/')[3]
-                                user_id = vk.utils.resolveScreenName(screen_name=user_nick)['object_id']
-                                user_in_list(users_list, user_id)
-                                if user_in_list(users_list, user_id):
-                                    if can_kick(users_list, event.obj.from_id, user_id, chat_id):
-                                        vk.messages.removeChatUser(chat_id=chat_id, member_id=user_id)
-                                        is_kicked = True
-                                else:
-                                    messages = 'Пользователь не участник беседы'
-                                    send_vk(messages, chat_id)
-                            else:
-                                messages = 'Неверная ссылка. Повторите попытку'
-                                send_vk(messages, chat_id)
-                        else:
-                            messages = 'Вы не ввели ссылку. Повторите попытку'
-                            send_vk(messages, chat_id)
+                        is_kicked = command_kick(event.obj.text, data_list)
+
     except Exception as exception:
         print('Exception - \n', exception)
         sleep(5)
